8831cc0570619c38555fa7ceff2c886ef6ea4d6f
sdfa66065-lang <sdfa66065@gmail.com>
2026-04-04 13:44:14 +0100

    commented out the merge conflict

--- demo/single_file_blending/test1/test_conflict.py
+++ demo/single_file_blending/test1/test_conflict.py
@@ -1,7 +1,10 @@
-def distill_context(ticket_id: str, target_ref: str):
-    log_telemetry("distill_context_called", ticket_id)
-    if cache.exists(ticket_id):
-        return cache.get(ticket_id)
-    jira_payload = fetch_jira_payload(ticket_id)
-    github_payload = fetch_github_payload(target_ref)
-    return jira_payload, github_payload
+# def distill_context(ticket_id: str):
+# <<<<<<< HEAD
+#     log_telemetry("distill_context_called", ticket_id)
+#     if cache.exists(ticket_id):
+#         return cache.get(ticket_id)
+#     return fetch_jira_payload(ticket_id)
+# =======
+# def distill_context(target_ref: str):
+#     return fetch_github_payload(target_ref)
+# >>>>>>> upstream/main
